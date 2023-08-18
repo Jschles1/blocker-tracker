a824bcacc8661360dc7e1f0a7dcc24a7ae5f53d0
Jschles1 <johnschlesinger5@gmail.com>
2023-08-18 13:54:51 -0400

    Navigation component set up

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,3 +1,5 @@
+import Navigation from "./navigation";
+
 type AppLayoutProps = {
   children: React.ReactElement;
 };
@@ -5,8 +7,8 @@ type AppLayoutProps = {
 export default function Layout({ children }: AppLayoutProps) {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-gray">
-      <header>Navigation</header>
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+      <Navigation />
+      <main className="flex w-full flex-1 flex-col items-center justify-center text-center">
         {children}
       </main>
     </div>
---
+++ src/components/navigation.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+export default function Navigation() {
+  return <header>Navigation</header>;
+}
